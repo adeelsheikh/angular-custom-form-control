87cc405f9cbf301be63c8f06e2c7679e6d484d29
adsheikh <adeelnayyersheikh@gmail.com>
2019-03-18 14:14:42 +0400

    rating component is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RatingInputComponent } from './components/rating-input/rating-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RatingInputComponent
   ],
   imports: [
     BrowserModule
